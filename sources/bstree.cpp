dbad6413b81842c5bd6b2f927d211f209e337be3
DolanL <32385280+DolanL@users.noreply.github.com>
2018-03-26 10:57:32 +0300

    Update bstree.cpp

--- sources/bstree.cpp
+++ sources/bstree.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 #include "BSTree.hpp"
+#include <string>
 using namespace std;
 using namespace BSTree;
+
+
 Tree::Tree() {
 	root = nullptr;
 }
-void Tree::add_node(int var,Node *&MyTree) { //Фукция добавления звена в дерево
-	if (nullptr==MyTree) {
-		MyTree=new Node;
-		MyTree->var=var;
-		MyTree->left=MyTree->right=nullptr;
+Node* Tree::add_node(int var, Node *&MyTree) { //Фукция добавления звена в дерево
+	if (nullptr == MyTree) {
+		MyTree = new Node{var,nullptr,nullptr};
 	}
-	if (var<MyTree->var) {
-		if (MyTree->left!=nullptr) add_node(var,MyTree->left);
-		else {
-			MyTree->left=new Node;
-			MyTree->left->left=MyTree->left->right=nullptr;
-			MyTree->left->var=var;
-		}
+	else{
+	if (var < MyTree->var)
+		MyTree->left=add_node(var, MyTree->left);
+	if (var>MyTree->var)
+		MyTree->right=add_node(var, MyTree->right);
 	}
-	if (var>MyTree->var) {
-		if (MyTree->right!=nullptr) add_node(var,MyTree->right);
-		else {
-			MyTree->right=new Node;  //Выделяем память правому подзвену
-			MyTree->right->left=MyTree->right->right=nullptr;
-			MyTree->right->var=var;
-		}
-	}
+	return MyTree;
 }
 void Tree::del(Node *&MyTree) {
-	if (MyTree!=nullptr) {
+	if (MyTree != nullptr) {
 		del(MyTree->left);  //Рекурсивная функция прохода по левому поддереву
 		del(MyTree->right); //Рекурсивная функция для прохода по правому поддереву
 		delete MyTree; //Удаляем конечный элемент дерева
@@ -37,7 +29,7 @@ void Tree::del(Node *&MyTree) {
 	}
 }
 
-void Tree::Insert (int var) {
+void Tree::Insert(Tree *&tree,int var) {
 	add_node(var, root);
 }
 void Tree::Show_Tree(Node *MyTree, int x) {
@@ -53,7 +45,8 @@ void Tree::Show_Tree(Node *MyTree, int x) {
 		cout << MyTree->var << endl;
 		if (MyTree->left != nullptr)
 			Show_Tree(MyTree->left, x + 1);
-	} else
+	}
+	else
 		cout << "Tree is empty!" << endl;
 }
 Tree::~Tree() {
