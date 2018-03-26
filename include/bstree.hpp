08a15a0e3f9818fcd8719a544aaca8e6321a674b
DolanL <32385280+DolanL@users.noreply.github.com>
2018-03-26 10:56:46 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -3,19 +3,19 @@ using namespace std;
 
 namespace BSTree {
 	struct Node {
-		int var; 
-		Node *left,*right; 
+		int var;
+		Node *left, *right;
 	};
 	class Tree {
-			Node *root;
 
-		public:
-			Tree();
-			void add_node(int var,Node *&MyTree);
-			void insert(int var);
-			void del(Node *&MyTree);
-			void Show_Tree(Node *MyTree, int x);
-			~Tree();
+	public:
+	    Node *root;
+		Tree();
+		Node* add_node(int var, Node *&MyTree);
+		void del(Node *&MyTree);
+		void Insert(Tree *&tree,int var);
+		void Show_Tree(Node *MyTree, int x);
+		~Tree();
 	};
 
 }
