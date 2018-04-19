13ba6303e4a8881a24f0cd3d643f32459c5dfa63
DolanL <32385280+DolanL@users.noreply.github.com>
2018-04-19 20:29:49 +0300

    Update bstree.cpp

--- sources/bstree.cpp
+++ sources/bstree.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "BSTree.hpp"
+#include "bstree.hpp"
 #include <string>
 using namespace std;
 using namespace BSTree;
@@ -8,48 +8,75 @@ using namespace BSTree;
 Tree::Tree() {
 	root = nullptr;
 }
-Node* Tree::add_node(int var, Node *&MyTree) { //Фукция добавления звена в дерево
-	if (nullptr == MyTree) {
-		MyTree = new Node{var,nullptr,nullptr};
+Node* Tree::insert(int var, Node *&node) { //Фукция добавления звена в дерево
+	if (nullptr == node) { // Если дерева нет, то ложим семечко
+		node = new Node{var,nullptr,nullptr}; // память под звено дерева
 	}
-	else{
-	if (var < MyTree->var)
-		MyTree->left=add_node(var, MyTree->left);
-	if (var>MyTree->var)
-		MyTree->right=add_node(var, MyTree->right);
+    else {
+	    if (var < node->var) //Если нововведенный элемент var меньше чем элемент var из семечка дерева, уходим влево
+		    node->left=insert(var, node->left);
+	    if (var> node->var) //Если нововведенный элемент var больше чем элемент var из семечка дерева, уходим вправо
+		    node->right=insert(var, node->right);
 	}
-	return MyTree;
+	return node;
 }
-void Tree::del(Node *&MyTree) {
-	if (MyTree != nullptr) {
-		del(MyTree->left);  //Рекурсивная функция прохода по левому поддереву
-		del(MyTree->right); //Рекурсивная функция для прохода по правому поддереву
-		delete MyTree; //Удаляем конечный элемент дерева
-		MyTree = nullptr;
+void Tree::remove(Node *&node) {
+	if (node != nullptr) {
+		remove(node->left);  //Рекурсивная функция прохода по левому поддереву
+		remove(node->right); //Рекурсивная функция для прохода по правому поддереву
+		delete node; //Удаляем конечный элемент дерева
+		node = nullptr;
 	}
 }
 
-void Tree::Insert(Tree *&tree,int var) {
-	add_node(var, root);
+void Tree::insert(int var) {
+	insert(var, root);
 }
-void Tree::Show_Tree(Node *MyTree, int x) {
+void Tree::show(){
+   show(root,1);
+}
+void Tree::show(char a){
+	show1(root);
+}
+void Tree::show(Node *node, int x) { // ф-я вывода
 
-	if (MyTree != nullptr) {
-		if (MyTree->right != nullptr)
-			Show_Tree(MyTree->right, x + 1);
+	if (node != nullptr) {
+		if (node->right != nullptr)
+			show(node->right, x + 1);
 		for (int i = 0; i < x; i++) {
 			cout << "   ";
 		}
-		if ((MyTree->var) != (root->var))
+		if ((node->var) != (root->var))
 			cout << "--";
-		cout << MyTree->var << endl;
-		if (MyTree->left != nullptr)
-			Show_Tree(MyTree->left, x + 1);
+		cout << node->var << endl;
+		if (node->left != nullptr)
+			show(node->left, x + 1 );
 	}
 	else
 		cout << "Tree is empty!" << endl;
 }
-Tree::~Tree() {
-	del(root);
+void Tree::show1(Node *node){
+    cout<<node->var<<" ";
+    if(node->left!=nullptr)
+      show1(node->left);
+    if(node->right!=nullptr)
+      show1(node->right);
 }
-
+void Tree::show2(Node *node){
+    if(node!=nullptr){
+      show2(node->left);
+    cout<<node->var<<" ";
+      show2(node->right);
+      }
+      return;
+      }
+void Tree::show3(Node *node){
+    if(node!=nullptr){
+      show3(node->left);
+      show3(node->right);
+    cout<<node->var<<" ";
+      }
+      }
+Tree::~Tree() {
+	remove(root);
+}
