c095bff115dd6d340345858189ae74eeea02d709
DolanL <32385280+DolanL@users.noreply.github.com>
2018-05-18 00:10:29 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -6,16 +6,17 @@ namespace BSTree {
 	struct Node {
 		int var; // то что записываем в дерево
 		Node *left, *right;
+		Node *parent;
 	};
 	class Tree {
 	private:
+		Node *root;
 	    Node* insert(int var, Node *&node);
 	    void show(Node *node, int x);
-	    void show1(Node *node);
-		void show2(Node *node);
-		void show3(Node *node);
+	    void direct_bypass(Node *node);
+		void symmetric_bypass(Node *node);
+		void back_bypass(Node *node);
 	public:
-		Node *root;
 		Tree();
 		void show(char choice);
 		void remove(Node *&node);
@@ -23,8 +24,8 @@ namespace BSTree {
 		void show();
 		void show23(Node *root,ofstream& fout);
 		void save_to_file();
-		//void show(Node *node,ostream& os, int x);
-		//friend auto operator<<(const Tree& tree,ostream& os) -> ostream&;
+		void show(Node *node,ostream& os, int x)const;
+	    friend auto operator<<(ostream& os,const Tree& tree) -> ostream&;
 		~Tree();
 	};
 }
